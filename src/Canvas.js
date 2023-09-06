12b448669cec5d50eb47930595d90715576354ca
JiaX Feng <499682030@qq.com>
2023-09-06 09:48:27 +0800

    1

---
+++ src/Canvas.js
@@ -0,0 +1,20 @@
+import { createCanvas } from './utils'
+
+// 画布类
+export default class Canvas {
+  constructor(width, height, opt) {
+    this.width = width
+    this.height = height
+    let { canvas, ctx } = createCanvas(width, height, opt)
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(-width / 2, -height / 2, width, height);
+    this.el = canvas
+    this.ctx = ctx
+  }
+
+  // 清除画布
+  clearCanvas() {
+    let { width, height } = this
+    this.ctx.clearRect(-width / 2, -height / 2, width, height)
+  }
+}
